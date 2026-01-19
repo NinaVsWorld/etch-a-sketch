2bac115c02402cf615ac82ba1b242e16dcdb73d6
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-01-19 21:48:34 +1100

    Complete function to generate etch a sketch grid

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function createGrid(squaresPerSide) {
+    const container = document.querySelector(".container");
+    const containerWidth = document.querySelector(".container").offsetWidth;
+    // For width of each square, get container width / squaresPerSide
+    const squareWidth = Math.round(containerWidth / squaresPerSide);
 
+    for (let i = 0; i < squaresPerSide; i++) {
+        for (let j = 0; j < squaresPerSide; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
+        }
+    }
+
+    document.querySelectorAll(".square").forEach((square) => {
+        square.style.width = squareWidth + "px";
+        square.style.height = squareWidth + "px";
+    });
 }
+
+createGrid(20);
