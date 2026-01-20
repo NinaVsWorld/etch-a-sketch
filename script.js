6691a8bdc87b20c229eb99f01e438ecefdd71d78
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-01-20 21:46:21 +1100

    Complete prompting the user for grid size, and function for resetting the grid after every new size specified

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Default grid size upon opening browser
+const DEFAULTGRIDSIZE = 10;
+
 function createGrid(squaresPerSide) {
     const container = document.querySelector(".container");
     const containerWidth = document.querySelector(".container").offsetWidth;
@@ -18,4 +21,30 @@ function createGrid(squaresPerSide) {
     });
 }
 
-createGrid(10);
+function resetGrid() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(element => {
+        element.parentNode.removeChild(element);
+    })
+}
+
+// Set default grid size
+createGrid(DEFAULTGRIDSIZE);
+
+// Set up event listener to get squares per side
+const submit = document.querySelector(".submit");
+submit.addEventListener("click", () => {
+    const inputValue = document.querySelector("input").value;
+    const errorMsg = document.querySelector(".error-msg");
+    const number = Number(inputValue);
+
+    if (!Number.isInteger(number)) {
+        errorMsg.textContent = "Please input whole numbers only!";
+    } else if (number < 1 || number > 100) {
+        errorMsg.textContent = "Please provide a number within the specified range!";
+    } else {
+        resetGrid();
+        createGrid(number);
+        console.log(number);
+    }
+})
